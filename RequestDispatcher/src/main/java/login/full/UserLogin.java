295e724ec14794d456d5a8bba9e3a0bf8ff07331
Venkat <Venkat.bca0@gmail.com>
2020-10-21 12:44:30 +0530

    Update

--- RequestDispatcher/src/main/java/login/full/UserLogin.java
+++ RequestDispatcher/src/main/java/login/full/UserLogin.java
@@ -43,7 +43,8 @@ public class UserLogin extends HttpServlet {
 		if (userCredentials.getUserCredentials().containsKey(name)
 				&& userCredentials.getUserCredentials().containsValue(password)) {
 
-			out.println("<div align=\"center\"> <b> Login Success ! </b></div>");
+			request.getRequestDispatcher("/show").forward(request, response);
+			
 		} else {
 			out.println("Invalid Credentials");
 			request.getRequestDispatcher("index.html").forward(request, response); // To forward the control to the
